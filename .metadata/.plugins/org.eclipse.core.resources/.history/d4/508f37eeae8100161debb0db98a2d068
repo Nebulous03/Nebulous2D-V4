0647f3eb5f02a6760c977bda8bfb41fc2779d6a6
NebulousDev <nebulousdev@gmail.com>
2016-09-23 21:38:57 -0400

    Starting Rending
    U0.0.4

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d4/508f37eeae8100161debb0db98a2d068
@@ -0,0 +1,9 @@
+package nebulous.utils;
+
+public interface LogicComponent {
+	
+	public void init();
+	public void update(double delta);
+	public void render();
+
+}
--- Nebulous2D_V4/src/nebulous/main/GameMain.java
+++ Nebulous2D_V4/src/nebulous/main/GameMain.java
@@ -1,5 +1,17 @@
 package nebulous.main;
 
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
+
+import nebulous.graphics.Mesh;
+import nebulous.graphics.Shader;
+import nebulous.utils.BufferUtilities;
+
 public class GameMain extends Game{
 
 	private static final int width = 1366;
@@ -7,6 +19,9 @@ public class GameMain extends Game{
 	private static final String title = "Nebulous2D Game Engine";
 
 	public static GameMain game = new GameMain();
+	
+	public Shader shader;
+	public Mesh mesh;
 	
 	public static void main(String[] args) {
 		game.getWindow().vSync(false);
@@ -19,22 +34,30 @@ public class GameMain extends Game{
 
 	@Override
 	public void init() {
+		shader = new Shader("/shaders/shader.vs", "/shaders/shader.fs");
 		
+		float[] vertices = new float[]{
+		        -0.5f,  0.5f, 0.0f,
+		        -0.5f, -0.5f, 0.0f,
+		         0.5f, -0.5f, 0.0f,
+		         0.5f,  0.5f, 0.0f,
+		    };
+		
+		mesh = new Mesh(vertices);
 	}
 	
-	int i = 0;
-
 	@Override
 	public void update() {
-		if(i == 100){
-			game.stop();
-		}
-		i++;
+		
 	}
 
 	@Override
 	public void render() {
-		
+	    shader.bind();
+	    
+	    mesh.render();
+	    
+	    shader.unbind();
 	}
 
 }
